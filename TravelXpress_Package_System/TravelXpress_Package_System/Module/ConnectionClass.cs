facbef1fe7b9bc821c0883753790061c4867e3ca
chiewck0702 <chiewchinkuan@gmail.com>
2025-06-26 00:03:59 +0800

    add back connection class file

---
+++ TravelXpress_Package_System/TravelXpress_Package_System/Module/ConnectionClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelXpress_Package_System.Module
+{
+    public class ConnectionClass
+    {
+        public string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Coding\\C#\\Travel_Package_System\\TravelXpress_Package_System\\TravelXpress_Package_System\\TravelXpressDBMS.mdf;Integrated Security=True";
+    }
+}
